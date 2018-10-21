249b5391ea3c2f051ce51a8f2254440d7ac73621
lucasflomuller <lucasflorianomuller@gmail.com>
2018-10-21 01:31:54 +0000

    add shuffle array method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import Navbar from './Navbar.jsx';
-import MemoryGame from './MemoryGame.jsx';
+import React, { Component } from "react";
+import Navbar from "./Navbar.jsx";
+import MemoryGame from "./MemoryGame.jsx";
 import "../styles/App.css";
 
 class App extends Component {
@@ -10,15 +10,16 @@ class App extends Component {
       numCards: 20,
       colors: new Set(),
       cards: []
-    }
+    };
   }
 
   generateHexColor = () => {
     // Generate number from min to max in hex
     const rand = Math.floor(Math.random() * 16777215)
-                    .toString(16).padStart(6, "0");
+      .toString(16)
+      .padStart(6, "0");
     return `#${rand}`;
-  }
+  };
 
   generateColorSet = () => {
     // Copiyng set
@@ -30,32 +31,48 @@ class App extends Component {
     }
     this.setState({ colors });
     return colors;
-  }
+  };
 
   generateCards = () => {
     this.generateColorSet();
     let cards = [...this.state.colors].map((color, index) => {
-      return ({
+      return {
         color,
         index,
         isShow: false
-      })
+      };
     });
-    cards = [...cards, ...cards];
+    cards = this.shuffleArray([...cards, ...cards]);
     this.setState({ cards });
-  }
+  };
 
-  shuffleArray = () => "implement"
+  // https://bit.ly/2gMXijX <- picked from this stack question
+  shuffleArray = array => {
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
 
-  render() { 
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  };
+
+  render() {
     return (
       <div className="container">
         <Navbar />
         <div className="game-menu">
-          <button 
-            className="btn btn-start"
-            onClick={this.generateCards}
-          >
+          <button className="btn btn-start" onClick={this.generateCards}>
             Start Game
           </button>
         </div>
