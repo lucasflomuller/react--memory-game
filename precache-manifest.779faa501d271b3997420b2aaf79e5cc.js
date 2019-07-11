c71cf3b8572f6b20fe16a2b055c938d0ef927d7b
lucasflomuller <lucasflorianomuller@gmail.com>
2019-07-11 23:53:00 +0000

    Updates

---
+++ precache-manifest.779faa501d271b3997420b2aaf79e5cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8f175c4fa580c01fb37",
+    "url": "/react--memory-game/static/js/runtime~main.e8f175c4.js"
+  },
+  {
+    "revision": "113785548a0327fc1faf",
+    "url": "/react--memory-game/static/js/main.11378554.chunk.js"
+  },
+  {
+    "revision": "60f3a0a506d1dfe70d56",
+    "url": "/react--memory-game/static/js/1.60f3a0a5.chunk.js"
+  },
+  {
+    "revision": "113785548a0327fc1faf",
+    "url": "/react--memory-game/static/css/main.b92e6394.chunk.css"
+  },
+  {
+    "revision": "41b17d5b35ad205fd159cbff73ebd6e4",
+    "url": "/react--memory-game/index.html"
+  }
+];
